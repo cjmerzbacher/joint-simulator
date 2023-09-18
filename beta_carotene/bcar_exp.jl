4145a5e45f2fa8e71ac0ea58b46a6263dc8e6c19
cjmerzbacher <cjmerzbacher@gmail.com>
2023-09-18 08:51:57 +0100

    updated experimental plan

--- beta_carotene/bcar_exp.jl
+++ beta_carotene/bcar_exp.jl
@@ -17,8 +17,8 @@ using Serialization
 using TreeParzen
 using CSV
 using LatinHypercubeSampling
-#home_path = "/home/cjmerzbacher/joint-simulator/" #for villarica runs
-home_path = "C:/Users/Charlotte/OneDrive - University of Edinburgh/Documents/research/joint-simulator/"
+home_path = "/home/cjmerzbacher/joint-simulator/" #for villarica runs
+#home_path = "C:/Users/Charlotte/OneDrive - University of Edinburgh/Documents/research/joint-simulator/"
 include(home_path * "models/beta_carotene.jl")
 include(home_path * "beta_carotene/bcar_sim.jl")
 
